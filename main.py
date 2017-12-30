bfcb2bf1c5f4b824d879187c7603fdef05bc6053
Jacqueline Xu <jackiestuy@gmail.com>
2017-12-30 17:41:48 -0500

    started with connections

--- main.py
+++ main.py
@@ -2,7 +2,55 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 
 app = Flask(__name__)
 
+@app.route('/')
+def root():
+	if in_session():
+		return redirect( url_for('home') )
+	else:
+		return render_template("welcome.html")
 
+@app.route('/home') #goes to home.html
+def home():
+	if in_session():
+		return render_template("home.html")
+	else:
+		return redirect( url_for('root') )
+
+@my_app.route('/login')
+def login():
+	if in_session():
+		return redirect( url_for('home') )
+	else:
+		return render_template("login.html")
+
+@app.route('/register')
+def register():
+	if in_session():
+		return redirect( url_for('home') )
+	else:
+		return render_template("register.html")
+
+
+@my_app.route('/store')
+def store():
+	if in_session():
+		return render_template("store.html")
+	else:
+		return redirect( url_for('login') )
+
+@app.route('/profile')
+def profile():
+	if in_session():
+		return render_template("profile.html")
+	else:
+		return redirect( url_for('login') )
+
+@my_app.route('/play')
+def play():
+	if in_session():
+		return render_template("play.html")
+	else:
+		return redirect( url_for('login') )
 
 if __name__ == '__main__':
 	app.debug = True

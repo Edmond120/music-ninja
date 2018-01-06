c8861255ffe8aef1fe31c335e39d8a0560513cd6
Jacqueline Xu <jackiestuy@gmail.com>
2018-01-06 10:21:24 -0500

    forms and displays

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def home():
 		# INFO to be passed: list of top 10 highcores
 		return render_template("home.html")
 	else:
-		return redirect( url_for('root') )
+		# make sure scores are in order from highest to lowest in the list
+		return redirect( url_for('root'), cash = 1000000, scores = {['apple', 1003], ['banana', 1002], ['cherry', 1000]} )
 
 
 @app.route('/login')
@@ -44,9 +45,9 @@ def auth():
         else:
                 flash("Sorry, the username already exist")
                 return render_template("register.html")
-        
-                
-        
+
+
+
 
 
 @app.route('/register')
@@ -60,8 +61,10 @@ def register():
 @app.route('/store')
 def store():
 	if in_session():
-		# INFO to be passed: search results from the query
-		return render_template("store.html", items = ['apple', 'banana', 'cherry'])
+		# INFO to be passed: search result from the query (should only be one)
+		# method=POST: if item purchased then subtract price of item from the amount of money the user has and refresh?
+		# method=GET: search query? you can rearrange if you want
+		return render_template("store.html", cash = 1000000, item = ['apple', 2.50])
 	else:
 		return redirect( url_for('login') )
 
@@ -71,7 +74,7 @@ def profile():
 	if in_session():
 		# INFO to be passed: items already bought by user and whether or not
 		# user has chosen to use it in gameplay
-		return render_template("profile.html")
+		return render_template("profile.html", cash = 1000000, items = {'apple': 0, 'banana': 1, 'cherry':1})
 	else:
 		return redirect( url_for('login') )
 
@@ -83,6 +86,8 @@ def play():
 	else:
 		return redirect( url_for('login') )
 
+@app.route('/logout')
+def logout():
 
 
 if __name__ == '__main__':

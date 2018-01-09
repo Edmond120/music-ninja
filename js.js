58d852ebf79998cb7d91f6d8bab9795eab1e4031
Kerry Chen <kchen36@stuy.edu>
2018-01-09 14:45:21 -0500

    attempting splitting

--- js.js
+++ js.js
@@ -70,7 +70,26 @@ class item extends entity{
     var ycord = 0;
     var velx = 0;
     var vely
-    
+
+    var splitleft = function(image) {
+	var width = image.width;
+	var height = image.height;
+	var canvas = document.createElement('canvas'),
+            ctx = canvas.getContext('2d');
+	
+	canvas.width = width;
+	canvas.height = height;
+	
+	ctx.drawImage(image, 0, 0, width, height,  0, 0, width, height);
+	var canvas2 = document.createElement('canvas2'),
+            ctx2 = canvas2.getContext('2d');
+	
+	canvas2.width = width;
+	canvas2.height = height;
+	
+	ctx2.drawImage(image, 51, 0, width, height,  0, 0, width, height);
+	
+	return [canvas,canvas2];
     var changevel = function(x,y){
 	velx = x;
 	vely = 
